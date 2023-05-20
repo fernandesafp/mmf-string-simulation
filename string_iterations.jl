fd42ed54d4cd04e561addaea5bd800d70f6ff9b8
André Fernandes <fernandesafp@gmail.com>
2023-05-20 21:55:29 +0100

    SymPy usage to plot iterations

--- string_iterations.jl
+++ string_iterations.jl
@@ -1,3 +1,4 @@
+using SymPy
 using Plots
 theme(:dark)
 
@@ -9,18 +10,20 @@ N = 15          # Maximum number of iterations
 fps = 3         # Frames per second
 
 # Fourier coefficient and function
-b(n) = 8*(-1)^n/(π*(2n+1))^2
-g(x, N) = sum([b(n) .* sin.((2n + 1).*x.*π/L) for n in 0:N])
+@syms n, x
+b = 8 / sympy.pi^2 * (-1)^n / (2n + 1)^2
+g = b * sympy.sin((2n + 1) * sympy.pi * x / L)
+
+display(g)
 
 # Animation function
-iterations = @animate for n in 0:N-1
-
-    plot(x, g(x,n), grid=false, framestyle=:semi,
-            linewidth=2, foreground_color_legend = nothing,
-            label="g(x) with N iterations = $(n)")
-    xaxis!((x₀,L), 0:1:2*L)
+iterations = @animate for iteration in 0:N-1
+    plot(summation(g, (n, 0, iteration)), grid=false, framestyle=:semi,
+        linewidth=2, foreground_color_legend=nothing,
+        label="g(x) with N iterations = $iteration")
+    xaxis!((x₀, L), 0:1:2*L)
     xlabel!("Length (m)")
-    yaxis!((x₀,1), 0:1:2*L)
+    yaxis!((x₀, 1), 0:1:2*L)
     ylabel!("Amplitude (m)")
 end
 
